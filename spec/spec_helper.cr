d8b1a92bf3765cb53f0f66dcb22850fbd9d2b5ba
Azzapop <aaron.b.hook@gmail.com>
2020-08-30 13:28:11 +1000

    Add profiling when running specs

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,6 +2,10 @@ require "../src/routes"
 require "../src/util/singleton"
 require "spectator"
 
+Spectator.configure do |config|
+  config.profile
+end
+
 def capture_action(&block : Api::Routes::Action)
   block
 end
